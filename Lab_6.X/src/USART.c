8d88f0d87956a8f88d91a5667549cea8724efaef
Jack Center <centerjd88@gmail.com>
2020-11-15 18:39:31 -0700

    added actions for specific commands to tx the command back and added error handling

--- Lab_6.X/src/USART.c
+++ Lab_6.X/src/USART.c
@@ -23,8 +23,8 @@
  * Global variables
  ******************************************************************************/
 char test = 0;
-char tx_string[STR_MAX];
-char tx_buffer[STR_MAX];
+char tx_string[STR_MAX + 25];
+//char error_str[STR_MAX + 25];
 char tx_pos = 0;
 char end_tx = 0;
 
@@ -41,6 +41,7 @@ void init_USART(void);
 void TxUsartHandler(void);
 void RxUsartHandler(void);
 void read_usart_str(void);
+void write_usart_str(void);
 
 /******************************************************************************
  * main()
@@ -51,7 +52,8 @@ void main(void) {
     while(1){
         if (new_rx == 1){
             read_usart_str();
-            new_rx = 0;
+            write_usart_str();
+            new_rx = 0;  
         }
     }
     
@@ -70,6 +72,8 @@ void init(){
 
 
 void init_USART(){
+//    memset(rx_string, '\0', sizeof(rx_string));
+    
     TRISCbits.TRISC7 = 1;           // RX
     TRISCbits.TRISC6 = 0;           // TX
     // TXSTA1 = 0b00100000;         // Tx on
@@ -78,7 +82,7 @@ void init_USART(){
     SPBRG1 = 12;
     RCSTA1 = 0b10010000;
    
-//    PIE1bits.TX1IE = 1;
+    PIE1bits.TX1IE = 1;
     PIE1bits.RC1IE = 1;
 }
 
@@ -100,26 +104,39 @@ void read_usart_str(){
         strncpy(tx_string, rx_string, sizeof(tx_string));
     }
     
-    else {
-        strncpy(tx_string, "BAD_CMD", sizeof(tx_string));
+    else {   
+        if (rx_string[STR_MAX - 1] != '\0' && rx_string[STR_MAX - 1] != '\n'){
+            strcpy(tx_string, "Error: command too long\n");
+            strncpy(tx_string, strcat(tx_string, rx_string), sizeof(tx_string));
+        }
+        
+        else{
+            strcpy(tx_string, "Error: unknown command, ");
+            strncpy(tx_string, strcat(tx_string, rx_string), sizeof(tx_string));
+        }
     }
-    
-    memset(rx_string, 0, sizeof(rx_string));
+    memset(rx_string, '\0', sizeof(rx_string));
+}
+
+
+void write_usart_str(){
+    TXREG1 = tx_string[0];      // load first char
+    end_tx = 0;                 // no longer at the end of a tx
+    tx_pos = 1;                 // next index to look at
+    TXSTA1bits.TXEN1 = 1;       // enable tx
 }
 
 
 void TxUsartHandler(){
+    
     if (end_tx == 1) {
-        tx_pos = 0;
-        // disable tx
-        // disable int or IF?
+        TXSTA1bits.TXEN1 = 0;
     }
     
     else{
-        TXREG1 = tx_buffer[tx_pos];     // should clear the flag by itself?
+        TXREG1 = tx_string[tx_pos];     // should clear the flag by itself?
         
-
-        if (tx_buffer[tx_pos] == '\n'){
+        if (tx_string[tx_pos] == '\n'){
             end_tx = 1;
         }
         
@@ -144,15 +161,8 @@ void RxUsartHandler(){
     
     rx_buffer[rx_pos] = RCREG1;     // should clear the flag by itself?
     
-    // TESTING
-//    TXREG1 = rx_buffer[rx_pos];
-//    TXSTA1 = 0b00100000;            // Tx on
-    
     if (rx_buffer[rx_pos] == '\n'){
         strncpy(rx_string, rx_buffer, rx_pos+1);
-//        while (++rx_pos < STR_MAX){      // fill rest of string with 
-//            rx_string[rx_pos] = 0;
-//        }
         rx_pos = 0;
         new_rx = 1;               
     }
